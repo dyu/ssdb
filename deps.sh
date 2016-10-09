b8442423e2aae83ebbc8ceac22fdb245cad8cf42
dyu <david.yu.ftw@gmail.com>
2016-10-09 18:04:48 +0800

    use hyperleveldb

--- deps.sh
+++ deps.sh
@@ -5,10 +5,9 @@ mkdir -p deps && cd deps
 GIT_CLONE="git clone --depth 1 --single-branch -b"
 
 if [ ! -e HyperLevelDB ]; then
-    $GIT_CLONE master https://github.com/rescrv/HyperLevelDB.git
+    $GIT_CLONE ssdb https://github.com/dyu-deploy/HyperLevelDB.git
     cd HyperLevelDB
     mkdir build
     autoreconf -i && ./configure --prefix=$PWD/build
-    cd include && ln -s hyperleveldb leveldb && cd ..
     cd ..
 fi
