97130c5209126505257ee6fa9a9e7660551a9822
dyu <david.yu.ftw@gmail.com>
2016-10-13 19:11:55 +0800

    comment out printf

--- src/proc_jni.cpp
+++ src/proc_jni.cpp
@@ -5,31 +5,31 @@
 int proc_getrange(NetworkServer *net, Link *link, const Request &req, Response *resp){
     JniContext *jni = resp->jni;
     int rpcId;
-    if (req.size() != 3 || 0 >= (rpcId = req[2].Int())) {
+    if (req.size() < 3 || 0 >= (rpcId = req[2].Int())) {
         resp->push_back("Invalid request");
         return 0;
     }
 
-    printf("rpc: %d\n", rpcId);
+    //printf("rpc: %d\n", rpcId);
 
     jni->env->CallStaticVoidMethod(jni->class_, jni->handle_, jni->type, jni->id);
-    resp->push_back("[{\"getrange\":true}]");
+    resp->push_back("[0]");
     return 0;
 }
 
 int proc_setrange(NetworkServer *net, Link *link, const Request &req, Response *resp){
     JniContext *jni = resp->jni;
     int rpcId;
-    if (req.size() != 4 || 0 >= (rpcId = req[2].Int())) {
+    if (req.size() < 4 || 0 >= (rpcId = req[2].Int())) {
         resp->push_back("Invalid request");
         return 0;
     }
 
     //const Bytes &payload = req[3];
 
-    printf("rpc: %d\n", rpcId);
+    //printf("rpc: %d\n", rpcId);
 
     jni->env->CallStaticVoidMethod(jni->class_, jni->handle_, jni->type, jni->id);
-    resp->push_back("[{\"setrange\":true}]");
+    resp->push_back("[0]");
     return 0;
 }
